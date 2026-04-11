6d908cb1fc51a60414bb29788e72584b3ca85007
dittakavigayatri-potlucks <dittakavigayatri@gmail.com>
2026-04-11 14:34:21 -0400

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,22 +6,20 @@ import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { Metadata } from "next";
 import Image from "next/image";
-
 export const metadata: Metadata = {
   title: "Contact | Gayatri Dittakavi",
   description:
     "Gayatri Dittakavi is a Quantitative Researcher focused on Macro-financial systems, Econometrics, and Stochastic Modeling. She works at the intersection of markets, policy, and data-driven decision making.",
 };
-
 export default function Projects() {
   return (
     <Container>
       <span className="text-4xl">✉️</span>
       <Heading className="font-black mb-2">Contact Me</Heading>
       <Paragraph className="mb-10 max-w-xl">
-        If you're interested in quantitative research, macro-financial analysis,
+        If you&apos;re interested in quantitative research, macro-financial analysis,
         or collaborations in Econometrics, Systematic Strategies, or DeFi systems,
-        feel free to reach out. I’m always open to discussing ideas, research, or opportunities.{" "}
+        feel free to reach out. I&apos;m always open to discussing ideas, research, or opportunities.{" "}
       </Paragraph>
       <Contact />
     </Container>
